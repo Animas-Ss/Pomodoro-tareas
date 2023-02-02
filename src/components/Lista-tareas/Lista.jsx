72cef1480ea08315c464447e3d11821221779e5d
Sebastián Sosa <ssosa@datamemory.local>
2023-02-02 17:16:47 -0300

    estilo a tarjetas de tareas

--- src/components/Lista-tareas/Lista.jsx
+++ src/components/Lista-tareas/Lista.jsx
@@ -5,17 +5,24 @@ import './Lista.css';
 export const Lista = () => {
 
     const settingsInfo = useContext(SettingsContext);
-    
+
     return (
         <div className='lista-tareas'>
-
             {settingsInfo.tareas.map((tarea) => (
-                <div className='tarea-realizada'>
-                    <div className='tarea-realizada-frente'>
-                      <h4>{tarea.tarea}</h4>
-                    </div>
-                    <div>
-
+                <div className='tarea-padre'>
+                    <div className='tarea-realizada'>
+                        <div className='tarea-realizada-frente'>
+                            <div className='frente-contenido'>
+                                <h4>{tarea.tarea}</h4>
+                            </div>
+                        </div>
+                        <div className='tarea-realizada-atras'>
+                            <div className='atras-contenido'>
+                                <h3>{tarea.asignacion}</h3>
+                                <p>{tarea.descripcion}</p>
+                                <button className='btn-tareas'>Modificar</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             ))}
